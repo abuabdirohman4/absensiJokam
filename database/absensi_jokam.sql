0ccc4018479126e78ae3385c543d94197184ad7a
bagusseno <bagusseno354@gmail.com>
2020-02-22 08:53:54 +0700

    + database

---
+++ database/absensi_jokam.sql
@@ -0,0 +1,197 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.0.2
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 22, 2020 at 02:52 AM
+-- Server version: 10.0.17-MariaDB
+-- PHP Version: 5.6.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `absensi_jokam`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `meta_index_peserta_pengajian`
+--
+
+CREATE TABLE `meta_index_peserta_pengajian` (
+  `id` int(10) NOT NULL,
+  `pengajian_title_id` int(10) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `meta_value_peserta_pengajian`
+--
+
+CREATE TABLE `meta_value_peserta_pengajian` (
+  `id` int(10) NOT NULL,
+  `peserta_pengajian_id` int(10) NOT NULL,
+  `meta_index_peserta_pengajian_id` int(10) NOT NULL,
+  `value` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengajian_event`
+--
+
+CREATE TABLE `pengajian_event` (
+  `id` int(10) NOT NULL,
+  `pengajian_title_id` int(10) NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengajian_title`
+--
+
+CREATE TABLE `pengajian_title` (
+  `id` int(10) NOT NULL,
+  `user_id` int(10) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `type` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `peserta_pengajian`
+--
+
+CREATE TABLE `peserta_pengajian` (
+  `id` int(10) NOT NULL,
+  `pengajian_title_id` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `presence`
+--
+
+CREATE TABLE `presence` (
+  `id` int(10) NOT NULL,
+  `pengajian_event_id` int(10) NOT NULL,
+  `peserta_pengajian_id` int(10) NOT NULL,
+  `status` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(10) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `meta_index_peserta_pengajian`
+--
+ALTER TABLE `meta_index_peserta_pengajian`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `meta_value_peserta_pengajian`
+--
+ALTER TABLE `meta_value_peserta_pengajian`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pengajian_event`
+--
+ALTER TABLE `pengajian_event`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pengajian_title`
+--
+ALTER TABLE `pengajian_title`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `peserta_pengajian`
+--
+ALTER TABLE `peserta_pengajian`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `presence`
+--
+ALTER TABLE `presence`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `meta_index_peserta_pengajian`
+--
+ALTER TABLE `meta_index_peserta_pengajian`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `meta_value_peserta_pengajian`
+--
+ALTER TABLE `meta_value_peserta_pengajian`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `pengajian_event`
+--
+ALTER TABLE `pengajian_event`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `pengajian_title`
+--
+ALTER TABLE `pengajian_title`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `peserta_pengajian`
+--
+ALTER TABLE `peserta_pengajian`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `presence`
+--
+ALTER TABLE `presence`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
